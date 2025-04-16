cd33aebe034351807ba06dcf7bd5ccb5a82a428b
NurbolUtegen <40791@iitu.edu.kz>
2025-04-16 17:14:31 +0500

    Изменил пользовательский сайт,создал вход и выход на аккаунт,исправил преждние ошибки,добавил систему бронировании и доступных машин для брона

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("myapp.urls")),
+    path('admin/', admin.site.urls),
+    path('api/', include('carlist.urls')),
+    path('', include('carlist.urls')),  # маршруты регистрации/главной страницы
+    path('accounts/', include('django.contrib.auth.urls')),  # логин/логаут
 ]
+
 
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from myapp.views import car_list, rent_car 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', car_list, name='car_list'),
     path('rent/<int:car_id>/', rent_car, name='rent_car'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
